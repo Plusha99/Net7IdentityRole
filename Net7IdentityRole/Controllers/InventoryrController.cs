0f0eabb1d21d77c72a737a9fc9029c6145e50b67
Plusha99 <109800436+Plusha99@users.noreply.github.com>
2023-01-06 14:51:26 +0300

    Added controllers

---
+++ Net7IdentityRole/Controllers/InventoryrController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Net7IdentityRole.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class InventoryController1 : Controller
+    {
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+    }
+}
